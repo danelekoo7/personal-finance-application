608920de506ca9a0be969f1f6ce39c505da125c7
danelekoo7 <danieljedrus@gmail.com>
2020-05-29 21:12:26 +0200

    * home page view created * prepared th:fragments

--- src/main/java/pl/jedrus/finance/configuration/SecurityConfiguration.java
+++ src/main/java/pl/jedrus/finance/configuration/SecurityConfiguration.java
@@ -1,8 +1,7 @@
-package pl.jedrus.finance.security;
+package pl.jedrus.finance.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +11,12 @@ import pl.jedrus.finance.service.SpringDataUserDetailsService;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    //temp disabled security
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
